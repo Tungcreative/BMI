d813502c5c340ba4a0b4ee08d92b4ed0b1199703
Tungcreative <tungbill033@gmail.com>
2023-09-17 21:43:30 +0700

    Hello

--- Buoi17.py
+++ Buoi17.py
@@ -2,19 +2,19 @@ import streamlit as st
 
 st.title('Welcome to BMI Caculator')
 can_nang = st.number_input('Hãy điền cân nặng của bạn bằng đơn vị kg')
-tinh_toan = st.radio('Hãy chọn cân nặng của bạn qua:', ['xăng-ti-mét', 'đề-xi-mét', 'mét'])
-chieu_cao = st.number_input('Hãy điền chiều cao của bạn')
-if st.button('Caculate BMI'):
-    calculate = can_nang / (chieu_cao * chieu_cao)
-    st.subheader('Chỉ số BMI của bạn là: '+ str(calculate))
-    if calculate <= 18.5:
+chieu_cao = st.number_input('Hãy điền chiều cao của bạn bằng đơn vị m')
+if st.button('Calculate BMI'):
+    bmi = can_nang / (chieu_cao * chieu_cao)
+    st.balloons()
+    st.subheader('Chỉ số BMI của bạn là: ' + str(bmi))
+    if bmi <= 18.5:
         st.info('Nhẹ cân')
-    if calculate >= 18.5 and calculate <= 24.9:
+    if bmi >= 18.5 and bmi <= 24.9:
         st.success('Bình thường')
-    if calculate >= 25 and calculate <= 30:
+    if bmi >= 25 and bmi <= 29.9:
         st.warning('Thừa cân')
-    if calculate >= 20 and calculate <= 34.9:
+    if bmi >= 30 and bmi <= 34.9:
         st.error('Béo phì độ 1')
-    if calculate >= 35:
+    if bmi >= 35:
         st.exception(can_nang('Béo phì độ 2 trở lên'))
 st.image('BMI.png')
